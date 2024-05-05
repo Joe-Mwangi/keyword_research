ed408bde9e63fb4a9994b0b3533637773988257b
Joe-Mwangi <joem99271@gmail.com>
2024-05-05 12:28:32 +0300

    icons

--- components/single-card.tsx
+++ components/single-card.tsx
@@ -1,16 +1,20 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Loader2 } from "lucide-react";
+import { GoogleIcon } from "./icons";
 
 type Props = {
   data: [];
   loading: boolean;
   title: string;
+  Icon: React.ReactElement;
 };
-export const ResultCard: React.FC<Props> = ({ data, loading, title }) => {
+export const ResultCard: React.FC<Props> = ({ data, loading, title, Icon }) => {
+  // if (data.length === 0) return <></>;
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-sm font-medium">{title}</CardTitle>
+        <CardTitle className="text-lg font-bold">{title}</CardTitle>
+        {Icon}
       </CardHeader>
       <CardContent>
         {loading ? (
