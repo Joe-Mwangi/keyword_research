512e03f8805f7953fac3343cf3e7d4a719ffe550
Joe-Mwangi <joem99271@gmail.com>
2024-05-05 13:01:31 +0300

    clipboard

--- components/single-card.tsx
+++ components/single-card.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Loader2 } from "lucide-react";
 import { GoogleIcon } from "./icons";
+import { ClipboardCopy } from "./copy";
 
 type Props = {
   data: [];
@@ -9,13 +10,13 @@ type Props = {
   Icon: React.ReactElement;
 };
 export const ResultCard: React.FC<Props> = ({ data, loading, title, Icon }) => {
-  // if (data.length === 0) return <></>;
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-lg font-bold">{title}</CardTitle>
         {Icon}
       </CardHeader>
+      {data.length === 0 ? null : <ClipboardCopy copyText={data.join(" ")} />}
       <CardContent>
         {loading ? (
           <Loader2 className="animate-spin" />
